99c26c23bfb9924c92154a4765219385690246d1
Jatin917 <jkchandel02@gmail.com>
2025-11-13 13:15:20 +0530

    enqueue part done

--- storage.py
+++ storage.py
@@ -13,11 +13,12 @@ class Storage:
     # Entry into the redis queue
     def enqueue(self, job: Job):
         key = f"{self.ns}:job:{job.id}"
-        self.r.hset(key, mapping=job.__dict__)
-        self.r.rpush(f"{self.ns}:queue:pending", job.id)
+        self._client.hset(key, mapping=job.__dict__)
+        self._client.rpush(f"{self.ns}:queue:pending", job.id)
 
-
-if __name__=="main":
+if __name__ == "__main__":
+    print("main function")
     job = Job.new("echo hi")
+    storage = Storage()
     storage.enqueue(job)
-    print(r.lrange("queuectl:queue:pending", 0, -1))
+    print(storage._client.lrange("queuectl:queue:pending", 0, -1))
